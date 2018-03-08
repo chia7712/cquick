e1a2e087a1b081100a6135e7f711eed257f93d3c
Chia-Ping Tsai <chia7712@gmail.com>
2018-03-08 11:44:33 +0800

    Support to build dist source -- fix05

--- bin/quickstart_kafka.sh
+++ bin/quickstart_kafka.sh
@@ -27,31 +27,23 @@ if [[ "${KAFKA_BRANCH}" == http* ]]; then
 	tar -zxvf $distpath -C /opt/kafka
     rm -f $distpath
   else
-    # build the binary from dist source
+    # prepare the source code
     tar -zxvf $distpath -C /tmp/
 	rm -f $distpath
 	sourcepath=$(find "/tmp/" -maxdepth 1 -type d -name "kafka*")
-	cd $sourcepath
-    gradle
-    ./gradlew clean
-    ./gradlew releaseTarGz -x signArchives
-    binarypath=$(find "$sourcepath/core/build/distributions/" -maxdepth 1 -type f -name "*.tgz")
-    tar -zxvf $binarypath -C /opt/kafka
-	cd ~/
-	rm -rf $sourcepath
+	# we will build the source later
   fi
 else
-  sourcepath=""
   # if the father docker has download the kafka source code, we use it directly.
   if [ -d "$KAFKA_SOURCE" ]; then
     sourcepath=$KAFKA_SOURCE
   else
-    sourcepath=/tmp/kafak
-	git clone https://github.com/apache/kafak $sourcepath
+    sourcepath=/tmp/kafka
+	git clone https://github.com/apache/kafka $sourcepath
   fi
   cd $sourcepath
   git checkout -- . | git clean -df
-  git checkout $HBASE_BRANCH
+  git checkout $KAFKA_BRANCH
   git pull
   if [ -f /testpatch/patch ]; then
     git apply /testpatch/patch --stat
@@ -59,6 +51,11 @@ else
   else
     echo "no patch file"
   fi
+  # we will build the source later
+fi
+# build the binary by source
+if [ -d "$sourcepath" ]; then
+  cd $sourcepath
   gradle
   ./gradlew clean
   ./gradlew releaseTarGz -x signArchives
@@ -67,10 +64,6 @@ else
   cd ~/
   rm -rf $sourcepath
 fi
-
-  gradle
-  ./gradlew clean
-  ./gradlew releaseTarGz -x signArchives
   
 # set kafka home
 KAFKA_ASSEMBLY=$(find "/opt/kafka" -maxdepth 1 -type d -name "kafka*SNAPSHOT")
@@ -84,12 +77,13 @@ echo "export JAVA_HOME=$JAVA_HOME" >> $HOME/.bashrc
 echo "export PATH=\$PATH:\$KAFKA_HOME/bin:\$ZOOKEEPER_HOME/bin" >> $HOME/.bashrc
 
 # deploy zookeeper config
-cp $HQUICK_HOME/conf/zookeeper/* $ZOOKEEPER_HOME/conf/
+cp $CQUICK_HOME/conf/zookeeper/* $ZOOKEEPER_HOME/conf/
 
 # start zookeeper
 $ZOOKEEPER_HOME/bin/zkServer.sh start
 
 # START kafka
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+# TODO: just run the kafka server in the background
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > /dev/null 2>&1 &
 
 exec bash
--- docker/kafka.quickstart.dockerfile
+++ docker/kafka.quickstart.dockerfile
@@ -3,9 +3,9 @@ FROM chia7712/kafka:base
 # INSTALL openssh
 RUN apt-get install -y openssh-server openssh-client
 
-# CLONE hquick
-RUN mkdir $COMPONENT_HOME/hquick
-RUN git clone https://github.com/chia7712/cquick.git $COMPONENT_HOME/hquick
+# CLONE cquick
+RUN mkdir $COMPONENT_HOME/cquick
+RUN git clone https://github.com/chia7712/cquick.git $COMPONENT_HOME/cquick
 
 # Set ENV
 ENV CQUICK_HOME=$COMPONENT_HOME/cquick
